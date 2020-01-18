866f0303c397bac2438a5f9973d9dd9309bbcaf9
ming <astro.toxic1203@gmail.com>
2020-01-18 12:59:38 +0700

    relocate file

--- number_reader.py
+++
@@ -1,7 +0,0 @@
-import json
-
-file_name = "number.json"
-f_obj = open(file_name, "r")
-numbers = json.load(f_obj)
-
-print(numbers)
--- number_writer.py
+++
@@ -1,8 +0,0 @@
-import json
-from random import randint
-
-number = [randint(0, 20) for x in range(10)]
-
-file_name = "number.json"
-f_obj = open(file_name, "w")
-json.dump(number, f_obj)
